23b5f1b133d607924818772926697a6ec68f29a2
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-06 15:23:31 -0500

    basic features

---
+++ MovieViewer/MovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieViewController.swift
+//  MovieViewer
+//
+//  Created by Subhay Manandhar on 2/5/17.
+//  Copyright © 2017 Subhay Manandhar. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
